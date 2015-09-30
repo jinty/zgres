4d7d2b30f10334913edeb407a30d9e3686f5e1b1
Brian Sutherland <brian@vanguardistas.net>
2015-09-30 10:04:42 +0200

    optionally launch with a callback

--- zgres/utils.py
+++ zgres/utils.py
@@ -3,11 +3,14 @@ import logging
 
 def exception_handler(loop, context):
     logging.error('Unexpected exception, exiting...')
+    # XXX: do we need to log the exception here?
     sys.exit(1)
 
-def run_asyncio():
+def run_asyncio(*callback_and_args):
     loop = asyncio.get_event_loop()
     loop.set_exception_handler(exception_handler)
+    if callback_and_args:
+        loop.call_soon(*callback_and_args)
     loop.run_forever()
     logging.info('Exiting after being asked to stop nicely')
     return 0
