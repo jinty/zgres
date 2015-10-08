3ef26d8ed23d9f9b906a9de2424a631c1eda7075
Brian Sutherland <brian@vanguardistas.net>
2015-10-08 11:12:00 +0200

    fix

--- zgres/apt.py
+++ zgres/apt.py
@@ -104,7 +104,7 @@ class AptPostgresqlPlugin:
         check_call(['pg_createcluster', self._version, self._cluster_name])
         self._assert_config()
         if self._superuser_connect_as:
-            check_call(['createuser', '-s', '-h', self._socket_dir(), '-p', self._port(), self._superuser_connect_as])
+            check_call(['sudo', '-u', 'postgres', 'createuser', '-s', '-h', self._socket_dir(), '-p', self._port(), self._superuser_connect_as])
 
     def postgresql_connect(self):
         return psycopg2.connect(database='postgres', user=self._superuser_connect_as, host=self._socket_dir(), port=self._port())
