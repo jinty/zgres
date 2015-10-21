f67fed88c4af0675a34dbf6ddd17a5e27d0a7108
Brian Sutherland <brian@vanguardistas.net>
2015-10-21 10:56:20 +0200

    try a real test of the apt plugin

--- zgres/tests/test_apt.py
+++ zgres/tests/test_apt.py
@@ -1,15 +1,15 @@
 import os
 from unittest import mock
 import asyncio
-from subprocess import check_output
+from subprocess import check_output, check_call
 
 import pytest
+import psycopg2
 from . import FakeSleeper
 
 def have_root():
-    print(dict(os.environ))
     destroy = os.environ.get('ZGRES_DESTROY_MACHINE', 'false').lower()
-    if destroy in ['t', 'true']: 
+    if destroy in ['t', 'true']:
         user = check_output(['whoami']).decode('latin1').strip()
         if user != 'root':
             raise Exception('I need to run as root if you want me to destroy the machine! I am {}'.format(repr(user)))
@@ -19,20 +19,25 @@ def have_root():
 needs_root = pytest.mark.skipif(not have_root(), reason='requires root and ZGRES_DESTROY_MACHINE=true in the environment')
 
 @pytest.fixture
-def plugin():
+def cluster():
+    return ('9.4', 'zgres-test')
+
+@pytest.fixture
+def plugin(cluster):
+    pg_version, cluster_name = cluster
     app = mock.Mock()
     app.config = dict(
             apt=dict(
-                postgresql_version=9.42,
-                postgresql_cluster_name='answers'))
+                postgresql_version=pg_version,
+                postgresql_cluster_name=cluster_name))
     from ..apt import AptPostgresqlPlugin
     return AptPostgresqlPlugin('zgres#apt', app)
 
-def test_config_file(plugin):
-    assert plugin._config_file(name='pg_hba.conf') == '/etc/postgresql/9.42/answers/pg_hba.conf'
+def test_config_file(plugin, cluster):
+    assert plugin._config_file(name='pg_hba.conf') == '/etc/postgresql/{}/{}/pg_hba.conf'.format(*cluster)
 
 @pytest.mark.asyncio
-async def test_monitoring(plugin):
+async def test_monitoring(plugin, cluster):
     with mock.patch('zgres.apt.sleep') as sleep, mock.patch('zgres.apt.call') as subprocess_call:
         retvals = [
                 0, # become healthy
@@ -54,10 +59,18 @@ async def test_monitoring(plugin):
                 mock.call.healthy(('zgres#apt', 'systemd')),
                 ]
         subprocess_call.assert_has_calls(
-                [mock.call(['systemctl', 'is-active', 'postgresql@9.42-answers.service'])] * len(retvals))
+                [mock.call(['systemctl', 'is-active', 'postgresql@{}-{}.service'.format(*cluster)]),
+                    ] * len(retvals))
 
 @needs_root
-def test_travis():
-    from subprocess import check_call
-    check_call(['ls', '/etc/postgresql'])
-    assert False, 'boom'
+def test_travis(plugin, cluster):
+    plugin.pg_initdb()
+    plugin.pg_start()
+    conn_info = plugin.pg_connect_info()
+    with psycopg2.connect(**conn_info) as conn:
+        with conn.cursor() as cur:
+            cur.execute('SELECT version(), current_database();')
+            got_ver, got_db = cur.fetchall()[0]
+    assert got_ver == cluster[0]
+    assert got_db == 'PostgreSQL {}'.format(cluster[1])
+    check_call(['pg_dropcluster'] + list(cluster))
