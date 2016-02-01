e569c809f093010209183112bd5a384f168e4dab
Brian Sutherland <brian@vanguardistas.net>
2016-02-01 10:17:35 +0100

    fix indentation and only depend on the zookeeper connection

--- zgres/show.py
+++ zgres/show.py
@@ -5,30 +5,43 @@ from pprint import pformat, pprint
 from .config import parse_args
 from .deadman import App, willing_replicas
 
+def indented_pprint(obj):
+    lines = []
+    for line in pformat(obj).splitlines(True):
+        lines.append('    ')
+        lines.append(line)
+    return ''.join(lines)
+
 def show_cli(argv=sys.argv):
     parser = argparse.ArgumentParser(description="Show zgres info")
     config = parse_args(parser, argv, config_file='deadman.ini')
     if config.has_section('deadman') and config['deadman'].get('plugins', '').strip():
-        # if deadman is configured show information about it's state
-        # HACK, we only need the plugins, really
         plugins = App(config)._plugins
         plugins.initialize()
+        all_state = list(plugins.dcs_list_state())
+        my_id = plugins.get_my_id()
+        my_state = None
+        for id, state in state:
+            if id == my_id:
+                my_state = state
+                break
+        # if deadman is configured show information about it's state
+        # HACK, we only need the plugins, really
         print('My State:')
-        print('    ID: {}'.format(plugins.get_my_id()))
-        print('    role: {}'.format(plugins.pg_replication_role()))
+        print('    ID: {}'.format(my_id))
+        print('    Replication role: {}'.format(my_state.get('replication_role')))
         print('Cluster:')
         print('    current master: {}'.format(plugins.dcs_get_lock_owner('master')))
         print('    database identifier: {}'.format(plugins.dcs_get_database_identifier()))
         print('    timeline: {}'.format(pformat(plugins.dcs_get_timeline())))
-        all_state = list(plugins.dcs_list_state())
         # willing_replicas is removed!
         willing = list(willing_replicas(all_state))
-        print('    willing replicas:')
-        pprint(willing)
+        print('\nwilling replicas:')
+        indented_pprint(willing)
         best_replicas = list(plugins.best_replicas(willing))
-        print('    best replicas:')
-        pprint(best_replicas)
-        print('    all conn info:')
-        pprint(list(plugins.dcs_list_conn_info()))
-        print('    all state:')
-        pprint(all_state)
+        print('\nbest replicas:')
+        indented_pprint(best_replicas)
+        print('\nall conn info:')
+        indented_pprint(list(plugins.dcs_list_conn_info()))
+        print('\nall state:')
+        indented_pprint(all_state)
